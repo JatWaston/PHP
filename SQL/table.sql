09bce5033ab10539f5650168f6860cbd18783355
kidreborn <ki.reborn@gmail.com>
2014-07-31 21:58:30 +0800

    添加创建数据库表的SQL

---
+++ SQL/table.sql
@@ -0,0 +1,5 @@
+﻿DROP TABLE IF EXISTS `t_user`;
+DROP TABLE IF EXISTS `lbs_bbs`;
+
+CREATE TABLE `t_user` (`user_account` VARCHAR(20),`user_pwd` VARCHAR(20),`user_name` VARCHAR(64),
+PRIMARY KEY(`user_account`));
